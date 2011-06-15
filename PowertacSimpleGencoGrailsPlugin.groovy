b1e33247d4d1381c0430d5dbc26d3e6d305bb23f
John Collins <jcollins@cs.umn.edu>
2011-06-14 21:10:24 -0500

    prep for release 0.3

--- PowertacSimpleGencoGrailsPlugin.groovy
+++ PowertacSimpleGencoGrailsPlugin.groovy
@@ -4,9 +4,9 @@ class PowertacSimpleGencoGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ['powertacCommon':'0.9 > *',
-                     'powertacServerInterface':'0.1 > *',
-                     'powertacRandom': '0.1 > *' ]
+    def dependsOn = ['powertacCommon':'0.10 > *',
+                     'powertacServerInterface':'0.2 > *',
+                     'powertacRandom': '0.2 > *' ]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
